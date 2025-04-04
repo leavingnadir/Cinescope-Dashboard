c6a302cb0edca6897463b2775a8315a6d8e64335
Hirusha79 <141672396+Hirusha79@users.noreply.github.com>
2025-04-04 21:26:22 +0530

    edit

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,13 @@
-import HeaderNav from "@/components/header-nav";
+import HeaderNav from "@/components/header-nav"; //alias import
 import Image from "next/image";
 
+//SSR Server Side Rendering
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <HeaderNav/>
-      <main className="bg-primary h-screen">
-        Our Main Section
-      </main>
-      <footer className="bg-amber-400 h-72">
-        Our New Footer
-      </footer>
+      <HeaderNav />
+      <main className="bg-primary h-screen">Our Main Section</main>
+      <footer className="bg-amber-400 h-72">Our New Footer</footer>
     </div>
   );
 }
