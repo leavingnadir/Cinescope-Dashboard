4c22f092a6c6d5b15c08c5d398cf0e2d1a3cdef3
Hirusha79 <141672396+Hirusha79@users.noreply.github.com>
2025-04-04 22:02:09 +0530

    edit

--- components/mode-toggle.jsx
+++ components/mode-toggle.jsx
@@ -1,3 +1,10 @@
+import { Sun } from "lucide-react";
+import { Button } from "./ui/button";
+
 export function ModeToggle() {
-  return <div>ModeToggle</div>;
+  return (
+    <Button variant="ghost" size="icon" className="h-9 w-9">
+      <Sun className="h-[1.2rem] w-[1.2rem]" />
+    </Button>
+  );
 }
