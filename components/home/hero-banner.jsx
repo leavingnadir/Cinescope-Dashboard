ef1c28343ec7aab6f710128a9f9c0e2120fa7789
Hirusha79 <141672396+Hirusha79@users.noreply.github.com>
2025-04-11 20:31:20 +0530

    Done

--- components/home/hero-banner.jsx
+++ components/home/hero-banner.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function HeroBanner() {
-  return <section></section>;
+  return (
+    <section className="relative overflow-hidden min-h-[70vh]">
+      <div className="bg-linear-to-r absolute inset-0 z-10 from-black/70 via-black/50 to-black/70"></div>
+      <div className="absolute inset-0 z-0 bg-[url('/images/hero-1.jpg')] bg-cover bg-center opacity-60 dark:opacity-40"></div>
+
+      <div className="container relative z-20 px-4 py-32">
+        <h1 className="text-white text-6xl font-bold text-center py-20">
+          Discover Amazing Movies
+        </h1>
+        <p className="text-white text-center text-xl">
+          Explore our best collection of movies all around the world
+        </p>
+      </div>
+    </section>
+  );
 }
