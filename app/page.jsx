5e1bcb6f8195dfbf38426c02b3b52a4cc3ec47cd
Hirusha79 <141672396+Hirusha79@users.noreply.github.com>
2025-04-11 19:56:45 +0530

    Edit

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import HeaderNav from "@/components/header-nav"; //alias import
+import HeroBanner from "@/components/home/hero-banner";
 import Image from "next/image";
 
 //SSR Server Side Rendering
@@ -6,7 +7,9 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderNav />
-      <main className="bg-primary h-screen">Our Main Section</main>
+      <main className="flex-1">
+        <HeroBanner />
+      </main>
       <footer className="bg-amber-400 h-72">Our New Footer</footer>
     </div>
   );
