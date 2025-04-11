b3f0e15e7cd7247ddc3d64a86d414dfe84495a05
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-04-11 20:16:25 +0530

    Add files via upload

--- components/home/hero-banner.jsx
+++ components/home/hero-banner.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HeroBanner() {
   return (
-    <section className="relative overflow-hidden min-h-[70vh] bg-red-500">
-      <div className="absolute inset-0 z-0 bg-green-500"></div>
+    <section className="relative overflow-hidden min-h-[70vh]">
+      <div className="absolute inset-0 z-0 bg-[url('/images/hero-1.jpg')] bg-cover bg-center opacity-60 dark:opacity-40"></div>
     </section>
   );
 }
