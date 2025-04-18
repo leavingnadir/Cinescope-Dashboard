c0f84978b966e4bb3da28668d9808af9bd899ac5
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-04-19 01:53:02 +0530

    Add files via upload

--- components/home/movies-list.jsx
+++ components/home/movies-list.jsx
@@ -1,4 +1,8 @@
+import Link from "next/link";
 import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
+import { MOVIES } from "@/lib/data";
 
 export default function MoviesList() {
   return (
@@ -8,9 +12,35 @@ export default function MoviesList() {
           <div className="relative">
             <Search className="text-primary/70 absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
 
-            {/*shadcn input*/}
+            {/* Shadcn Input Field */}
+            <Input
+              placeholder="Search movies by title or director"
+              className="border-primary/20 pl-9"
+            />
           </div>
         </div>
+      </div>
+
+      {/* Movies Cards Here */}
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {/* Loop Movies */}
+        {MOVIES.map((movie) => (
+          <div key={movie.id} className="">
+            <Link href={"/movies/${movie.id}"}>
+              <Card className="border-primary/20 hover:border-primary/50 overflow-hidden py-0 transition-colors">
+                <div className="aspect-2/3 w-full overflow-hidden">
+                  <Image
+                    width={300}
+                    height={450}
+                    src={movie.poster || "./placeholder.svg"}
+                    alt={movie.title}
+                    className="h-full w-full object-cover transition duration-300 hover:sclae-105"
+                  />
+                </div>
+              </Card>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
