98068a0b88628962215be4675ed37312ddd2d1f2
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-04-19 01:01:15 +0530

    Add files via upload

--- components/home/movies-list.jsx
+++ components/home/movies-list.jsx
@@ -1,3 +1,15 @@
+import { Search } from "lucide-react";
+
 export default function MoviesList() {
-  return <div>MoviesList</div>;
+  return (
+    <div className="space-y-6">
+      <div className="border-primary/20 bg-card shadow-xs rounded-lg border p-4">
+        <div className="flex flex-col gap-4">
+          <div className="relative">
+            <Search className="text-primary/70 absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
